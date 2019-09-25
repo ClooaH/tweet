2032874c7690d82147af2d5cb97f17bc8ddc5d2b
ClooaH <a.s.d.w.a.s@hotmail.com>
2019-09-25 09:36:16 +0200

    Added seperation in head tag.

--- views/index_layout.php
+++ views/index_layout.php
@@ -1,19 +1,11 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/main.css">
-    <title>Twat</title>
-</head>
-<body>
+<?php include 'head.php' ?>
     <div class="container px-5">
         <div class="container">
             <?php foreach ($result as $row): ?>
                 <div class="card mb-3" style="max-width: 540px;">
                     <div class="row no-gutters">
                     <div class="col-md-4">
-                        <img src="https://robohash.org/ <?= $row ['name'] ?>" class="card-img" alt="Unlucky. Rikti bild pöjk.">
+                        <img src="http://robohash.org/<?= $row ['name'] ?>" class="card-img" alt="Unlucky. Rikti bild pöjk.">
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
